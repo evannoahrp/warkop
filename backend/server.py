cf91acaaf004c45a9683b1f983ca66db57b63525
Evannoah <evannoah11@gmail.com>
2021-04-15 08:15:48 +0700

    file server commit

---
+++ backend/server.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello():
+    return "Hello, how are you?"
+
+if __name__ == "__name__":
+    print(Starting Python Flask Server For Warkop System)
