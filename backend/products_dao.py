54e827427d30cd0784de06f1931c5407f37b9a3f
Evannoah <evannoah11@gmail.com>
2021-04-15 08:02:05 +0700

    delete commit

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -30,18 +30,24 @@ def insert_new_product (connection, product):
     query = ("INSERT INTO products "
     "(name, unit_id, price_per_unit) "
     "VALUES (%s, %s, %s)")
-    data = (product['product_name'], product['item_id'], product['price_per_unit'])
+    data = (product['product_name'], product['unit_id'], product['price_per_unit'])
     cursor.execute(query, data)
     connection.commit()
 
     return cursor.lastrowid
 
+def delete_product(connection, product_id):
+    cursor = connection.cursor()
+    query = ("DELETE From products WHERE product_id = " + str(product_id))
+    cursor.execute(query)
+    connection.commit()
 
 if __name__ == '__main__':
     connection = get_sql_conncetion()
     #print(get_all_product(connection))
-    print(insert_new_product(connection, {
-        'product_name': 'jahe',
-        'unit_id': '2',
-        'price_per_unit': '5000'
-    }))
+    #print(insert_new_product(connection, {
+    #    'product_name': 'jahe',
+    #    'unit_id': '2',
+    #    'price_per_unit': '5000'
+    #}))
+    print(delete_product(connection, 14))
